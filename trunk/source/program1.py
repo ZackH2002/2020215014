d69aa07d1ee45a9a0d78ce21446e29f1da5bf1d0
hdh25490 <13896922098@163.com>
2023-11-24 14:26:08 +0800

    Modify program1 - First commit

---
+++ trunk/source/program1.py
@@ -0,0 +1,7 @@
+# program1.py
+def greet(name):
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    greet(user_name)
---
+++ trunk/source/program3.py
@@ -0,0 +1,15 @@
+# program3.py
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    if is_prime(num):
+        print(f"{num} is a prime number.")
+    else:
+        print(f"{num} is not a prime number.")
